f9ab528bd8e54d702dc82625d410d2933ce95481
Supratim Ghose <supratim7ghose@gmail.com>
2024-11-10 23:27:55 +0530

    Flask CORS dependency added

--- src/app.py
+++ src/app.py
@@ -6,7 +6,7 @@ import logging
 from sklearn.exceptions import NotFittedError
 
 app = Flask(__name__)
-CORS(app, resources={r"/predict": {"origins": "http://localhost:5173"}})
+CORS(app)
 
 logging.basicConfig(level=logging.INFO)
 
